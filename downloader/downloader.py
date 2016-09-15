bd726738bb8813942db8934bb79869823c2877a4
Daniele Ding <dinghino@gmail.com>
2016-09-15 12:27:45 +0200

    create module for downloader

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -2,8 +2,6 @@
 # encoding: utf-8
 
 import os
-import sys
-
 import urllib2
 import re
 
@@ -22,6 +20,7 @@ _img_url_regex = re.compile(r'<IMG src="(/comics/images/.*\.(?:gif|png))">')
 # Latest comic downloaded regexp.
 _img_filename = re.compile(r'oots(\d{4})\.(?:gif|png)')
 
+
 def open_page(url):
     """
     Simpy download a content and return it as a string.
@@ -30,6 +29,7 @@ def open_page(url):
     page = urllib2.urlopen(url).read()
     log.debug('"%s": download finished', url)
     return page
+
 
 def get_range(homepage='http://www.giantitp.com/Comics.html', directory='.'):
     """
@@ -40,7 +40,10 @@ def get_range(homepage='http://www.giantitp.com/Comics.html', directory='.'):
         p = open_page(homepage)
         # Find the latest comic index into the homepage.
         try:
-            _l = int(re.search(r'<A href="/comics/oots(\d+)\.html" class="SideBar">', p).group(1))
+            _l = int(re.search(
+                r'<A href="/comics/oots(\d+)\.html" class="SideBar">',
+                p).group(1))
+
             log.info('Latest available comic index: %d', _l)
             return _l
         except:
@@ -50,7 +53,10 @@ def get_range(homepage='http://www.giantitp.com/Comics.html', directory='.'):
     def _last_downloaded(directory):
         log.debug('Seraching for the latest downloaded comic index')
         try:
-            _downloaded = filter(lambda x: _img_filename.match(x), os.listdir(directory))[-1]
+            _downloaded = filter(
+                lambda x: _img_filename.match(x),
+                os.listdir(directory))[-1]
+
             _downloaded = int(_img_filename.match(_downloaded).group(1))
             log.info('Latest downloaded comic index: %d', _downloaded)
         except IndexError:
@@ -60,12 +66,13 @@ def get_range(homepage='http://www.giantitp.com/Comics.html', directory='.'):
 
     return _last_downloaded(directory), _latest(homepage)
 
+
 def get_image(n):
     """
     Downlaod the comic identified by the id n.
     """
     def _get_comic_page(n):
-        return open_page('http://www.giantitp.com/comics/oots%04d.html' %n)
+        return open_page('http://www.giantitp.com/comics/oots%04d.html' % n)
 
     def _get_img_url(page):
         return 'http://www.giantitp.com' + _img_url_regex.search(page).group(1)
@@ -76,20 +83,21 @@ def get_image(n):
     log.info('Download finished')
     return img, os.path.splitext(img_url)[1]
 
+
 def save_image(img, filename):
     """
     Save the image into file "filename".
     """
     log.info('Writing %s', filename)
-    open(filename, 'w').write(img)
+    open('./comics/' + filename, 'w').write(img)
     log.info('%s written', filename)
 
 
-def main():
-    last_downloaded, last = get_range()
+def run():
+    last_downloaded, last = get_range(directory='./comics')
     for i in range(last_downloaded + 1, last + 1):
         img, ext = get_image(i)
-        save_image(img, 'oots%04d%s' %(i, ext))
+        save_image(img, 'oots%04d%s' % (i, ext))
 
 if __name__ == '__main__':
-    main()
+    run()
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from downloader import downloader
+
+downloader.run()
